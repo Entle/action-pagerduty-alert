3006fd6d86fc49421c720db3258f39e84881c825
Johannes Jonker <johannes@entle.co>
2021-02-03 09:29:10 +0200

    First version

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# action-pagerduty-alert
+# PagerDuty Alert GitHub Action
+Sends a critical PagerDuty alert, e.g. on action failure.
+
+## Prerequisites
+
+1. Create a service integration in PagerDuty:
+    1. Go to PagerDuty > "Services" > Pick your service > "Integrations" > "Add a new integration"
+    2. Choose a name (e.g. "Your GitHub CI/CD") and "Use our API directly" with "Events API v2"
+    3. Copy the integration key
+2. Set up a secret in your GitHub repo to store the integration key, e.g. "PAGERDUTY_INTEGRATION_KEY"
+
+## Inputs
+
+`pagerduty-integration-key`
+
+**Required:** the integration key for your PagerDuty service
+
+## Example usage
+
+In your `steps`:
+
+```yaml
+- name: Send PagerDuty alert on failure
+  if: ${{ failure() }}
+  uses: Entle/action-pagerduty-alert@0.1.0
+  with:
+    pagerduty-integration-key: '${{ secrets.PAGERDUTY_INTEGRATION_KEY }}'
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+const core = require('@actions/core');
+const { context } = require('@actions/github');
+const axios = require('axios');
+
+// Trigger the PagerDuty webhook with a given alert
+async function sendAlert(alert) {
+  try {
+    const response = await axios.post('https://events.pagerduty.com/v2/enqueue', alert);
+
+    if (response.status === 202) {
+      console.log(`Successfully sent PagerDuty alert. Response: ${JSON.stringify(response.data)}`);
+    } else {
+      core.setFailed(
+        `PagerDuty API returned status code ${response.status} - ${JSON.stringify(response.data)}`
+      );
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+// Run the action
+try {
+  const integrationKey = core.getInput('pagerduty-integration-key');
+
+  sendAlert({
+    payload: {
+      summary: `${context.repo.repo}: Error in "${context.workflow}" run by @${context.actor}`,
+      timestamp: new Date().toISOString(),
+      source: 'GitHub Actions',
+      severity: 'critical',
+      custom_details: {
+        run_details: `https://github.com/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.runId}`,
+        related_commits: context.payload.commits
+          ? context.payload.commits.map((commit) => `${commit.message}: ${commit.url}`).join(', ')
+          : 'No related commits',
+      },
+    },
+    routing_key: integrationKey,
+    event_action: 'trigger',
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
