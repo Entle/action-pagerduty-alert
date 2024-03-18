308993fecd84cfb33026698f81d9a5fa3168f220
Heiko Voigt <heiko.voigt@jimdo.com>
2024-03-18 17:17:08 +0100

    add runbook url to pagerduty custom information
    So the on-call team knows where to find information about the service
    they are supposed to repair.

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ try {
         related_commits: context.payload.commits
           ? context.payload.commits.map((commit) => `${commit.message}: ${commit.url}`).join(', ')
           : 'No related commits',
+        runbook: core.getInput('runbook-url'),
       },
     },
     routing_key: integrationKey,
